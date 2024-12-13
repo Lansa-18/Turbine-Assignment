6c52bbbd2b68d19f8845c06a4f69ba176388dd90
Mapelujo <olamideiyanda18@gmail.com>
2024-12-13 06:52:12 +0100

    Revert "committing first assignment"
    This reverts commit 8ed88ac634701c90d1a5ac1326e6d113416d59db.

--- airdrop/keygen.ts
+++
@@ -1,7 +0,0 @@
-import { Keypair } from "@solana/web3.js";
-//Now we're going to create a new Keypair, like so:
-
-//Generate a new keypair
-let keypair = Keypair.generate()
-console.log(`You've generated a new Solana wallet: ${keypair.publicKey.toBase58()}`);
-console.log(`Solana Wallet Secret Key: [${keypair.secretKey}]`);
